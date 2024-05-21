0fc10c19d067f5592e477aa50a7efe634171929d
Vink64 <161389498+Vink64@users.noreply.github.com>
2024-05-21 00:40:38 +0000

    git actions performance

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((req,res)=>{
     })
 })
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
